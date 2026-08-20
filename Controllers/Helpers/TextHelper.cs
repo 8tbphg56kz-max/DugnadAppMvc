b3adbed4916a66b7d3146e0d7a0d99c5501828b9
53knut <134359859+53knut@users.noreply.github.com>
2026-08-20 15:58:10 +0200

    Endret tekstformatering, linjeskift

---
+++ Controllers/Helpers/TextHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace DugnadAppMvc.Helpers
+{
+    public static class TextHelper
+    {
+        public static string FormaterLinjeskift(string? tekst)
+        {
+            if (string.IsNullOrWhiteSpace(tekst))
+            {
+                return string.Empty;
+            }
+
+            // Normaliserer alle typer linjeskift
+            var linjer = tekst
+                .Replace("\r\n", "\n")
+                .Replace("\r", "\n")
+                .Split('\n');
+
+            // Fjern tomme linjer og mellomrom rundt hver linje
+            var rensedeLinjer = linjer
+                .Select(linje => linje.Trim())
+                .Where(linje => !string.IsNullOrWhiteSpace(linje))
+                .Select(linje => WebUtility.HtmlEncode(linje));
+
+            // Ett <br /> per faktisk linjeskift
+            return string.Join("<br />", rensedeLinjer);
+        }
+    }
+}
